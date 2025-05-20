350d0b0ff41e6d6459bddd1de33287191ffee02c
Jimmy <yanzhi@enso-ai.com>
2025-05-20 02:08:44 -0700

    process the result

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,26 @@
 {/* app/components/VideoPlayer.jsx */}
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { apiClient } from '../service/backend';
 import './VideoPlayer.css';
 
-export default function VideoPlayer({ url, timestamp }) {
+export default function VideoPlayer({ payload }) {
+  const { taskId, status, createdAt, timestamp } = payload;
+  const [videoUrl, setVideoUrl] = useState(null);
+
+  useEffect(() => {
+    if (taskId) {
+      apiClient.pollTaskUntilComplete(taskId)
+        .then((result) => {
+          console.log("poll task result:", result);
+          const video = result.videos[0]
+          setVideoUrl(video.url)
+        })
+        .catch((error) => {
+          console.error("Error polling task:", error);
+        });
+    }
+  }, [taskId])
+
   return (
     <div className="video-item">
       <div className="video-player">
