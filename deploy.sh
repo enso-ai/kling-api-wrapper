4fe53511b39c37206900fca0c52f00d8c9c4f219
yanzhi <yanzhi@enso-ai.com>
2025-05-23 20:24:59 -0700

    fix typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-gcloud run deploy kling-client-next \                                                              SIGINT   main 
-  --source . \
-  --region us-west1 \
-  --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com"
+gcloud run deploy kling-client-next \
+    --source . \
+    --region us-west1 \
+    --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com"
