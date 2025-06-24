fff0d04eb610c1a3722e5037fc3dc5b621700515
yanzhi <yanzhi@enso-ai.com>
2025-06-23 23:34:39 -0700

    format page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 'use client'
 
-import React from 'react';
 import SidePanel from '../components/SidePanel';
 import VideoGrid from '../components/VideoGrid';
 import '../styles/kling-ui.css';
 import { VideoProvider } from '../context/VideoContext';
 
 export default function Home() {
-  return (
-    <VideoProvider>
-      <div className="kling-container">
-        <SidePanel />
-        <VideoGrid />
-      </div>
-    </VideoProvider>
-  );
+    return (
+        <VideoProvider>
+            <div className='kling-container'>
+                <SidePanel />
+                <VideoGrid />
+            </div>
+        </VideoProvider>
+    );
 }
