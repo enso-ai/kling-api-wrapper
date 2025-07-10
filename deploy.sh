5294b8a2a0158f3fcaad7a40d060897062963e5a
yanzhi <yanzhi@enso-ai.com>
2025-07-09 19:55:18 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 gcloud run deploy kling-client-next \
     --source . \
     --region us-west1 \
-    --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com"
+    --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com" \
+    --service-account "playground@pure-lantern-394915.iam.gserviceaccount.com" \
+    --set-secrets="OPENAI_API_KEY=playground-openai-secret:latest"
+
